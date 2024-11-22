1ca09040959a93fa0e945f1ab1c71a3169a2e1a0
Mathieu <mathieu.dubart@edu.gobelins.fr>
2024-11-22 15:11:30 +0100

    Initial Commit

---
+++ AutoPictureAndRecognition/AutoPictureAndRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoPictureAndRecognitionApp.swift
+//  AutoPictureAndRecognition
+//
+//  Created by Mathieu DUBART on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct AutoPictureAndRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
